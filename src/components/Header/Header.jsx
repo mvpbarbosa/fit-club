ab4facd44fc6adf97d16d580ec2dc46de48bf881
Marcos Barbosa <mvpbarbosa16@gmail.com>
2023-05-29 17:54:01 -0300

    (feat): Creating and styling `Hero Component` #1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,14 @@ const Header = () => {
   return (
     <div className="header">
       <img src={Logo} alt="" className="logo" />
+
+      <ul className="header-menu">
+        <li>Home</li>
+        <li>Programs</li>
+        <li>Why us</li>
+        <li>Plans</li>
+        <li>Testimonials</li>
+      </ul>
     </div>
   );
 };
